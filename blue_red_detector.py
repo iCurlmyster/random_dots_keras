05bdf91bc1c13f18c75dd41a3507dcdf1d9fac51
Joshua Matthews <joshuamatthews@jmatth11.local>
2016-08-09 20:25:49 -0500

    Creating repository to push to github

---
+++ blue_red_detector.py
@@ -0,0 +1,79 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+import numpy as np
+import csv
+
+np.random.seed(7)
+
+with open("./blue_red_sample.csv",newline='') as csvfile:
+	f_read = csv.reader(csvfile, delimiter=',')
+	dataset = []
+	for row in f_read:
+		dataset.append(np.array(row))
+
+dataset = np.array(dataset)
+
+x = dataset[:,1:3]
+y = dataset[:,0]
+y = [1.0 if "blue" in x else 0.0 for x in y]
+y = np.array(y)
+
+model = Sequential()
+
+# allow first layer to use sigmoid. (gives better results)
+model.add(Dense(10, input_dim=2, init="uniform", activation="sigmoid"))
+
+#perform dropout of neurons at 0.4 threshold
+model.add(Dropout(0.4))
+
+# only use relu in middle layer
+model.add(Dense(8, init="uniform", activation="relu"))
+
+# also dropout neurons if below 0.3
+model.add(Dropout(0.3))
+
+model.add(Dense(1, init="uniform", activation="sigmoid"))
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(x, y, nb_epoch=150, batch_size=10)
+
+del dataset
+with open("./blue_red_sample_2.csv",newline='') as csvfile:
+	f_read = csv.reader(csvfile, delimiter=',')
+	dataset = []
+	for row in f_read:
+		dataset.append(np.array(row))
+
+dataset = np.array(dataset)
+
+x2 = dataset[:,1:3]
+y2 = dataset[:,0]
+y2 = [1.0 if "blue" in x else 0.0 for x in y2]
+y2 = np.array(y2)
+
+print("\nevaluating data that was trained on.")
+scores = model.evaluate(x, y, batch_size=56)
+print("\n{0}: {1}".format(model.metrics_names[1], scores[1]*100))
+print("{0}: {1}\n".format(model.metrics_names[0], scores[0]*100))
+
+print("\nevaluating new random data that model hasn't seen.")
+scores = model.evaluate(x2, y2, batch_size=56)
+print("\n{0}: {1}".format(model.metrics_names[1], scores[1]*100))
+print("{0}: {1}\n".format(model.metrics_names[0], scores[0]*100))
+
+print("predict 34")
+print(model.predict(x[34].reshape(1,2)))
+print("actual value {0}\n".format(y[34]))
+
+print("predict 35")
+print(model.predict(x[35].reshape(1,2)))
+print("actual value {0}".format(y[35]))
+
+predict_array = model.predict(x)
+
+print("\nPrinting predicted values 0-100 to file br_results.txt")
+with open("br_results.txt","w") as new_file:
+	for val in predict_array:
+		new_file.write("{0}\n".format(val[0]))
+
---
+++ blue_red_graph.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import csv
+import sys
+
+file_name = "./blue_red_sample.csv"
+if len(sys.argv) > 1:
+	file_name = sys.argv[1]
+
+with open(file_name,newline="") as csvReader:
+	f_read= csv.reader(csvReader, delimiter=",")
+	br_data = []
+	for row in f_read:
+		br_data.append(row)
+
+
+with open("./br_results.txt",newline="") as br_file:
+	f_read = csv.reader(br_file)
+	predict_data = []
+	for row in f_read:
+		predict_data.append(row)
+
+b_x = []
+b_y = []
+r_x = []
+r_y = []
+m_x = []
+m_y = []
+y_x = []
+y_y = []
+
+
+print("Threshold at 70% for deciding blue or red. Above 70% is blue otherwise red.")
+for val in range(len(predict_data)):
+	if float(predict_data[val][0]) > 0.70:
+		if "blue" in br_data[val][0]:
+			b_x.append(br_data[val][1])
+			b_y.append(br_data[val][2])
+		else:
+			m_x.append(br_data[val][1])
+			m_y.append(br_data[val][2])
+	else:
+		if "red" in br_data[val][0]:
+			r_x.append(br_data[val][1])
+			r_y.append(br_data[val][2])
+		else:
+			y_x.append(br_data[val][1])
+			y_y.append(br_data[val][2])
+
+print("Blue dots: Correctly labeled blue dots.\nRed dots: Correctly labeled red dots.")
+print("Magenta dots: Dots that should be labeled red but were identified as blue.")
+print("Yellow dots: Dots that should be labeled blue but were identified as red.")
+
+plt.plot(b_x, b_y, 'bo')
+plt.plot(r_x, r_y, 'ro')
+plt.plot(m_x, m_y, 'mo')
+plt.plot(y_x, y_y, 'yo')
+plt.show()
